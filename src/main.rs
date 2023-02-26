aef0e7a7e0158e76340e01d67ca7f55eb4dfcb83
MitchWijt <mitchel@wijt.net>
2023-02-26 18:39:03 +0100

    add first commit in pr

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate core;
 
 use clap::{Parser, Subcommand};
 use serde_json::Value;
+use serde_json::json;
 use crate::git_data::GitData;
 
 #[derive(Parser)]
@@ -40,16 +41,24 @@ async fn main() -> reqwest::Result<()> {
     Ok(())
 }
 
-async fn call_gh_api(github_data: GitData, token: String) -> reqwest::Result<String> {
-    let body = r#"{"title":"Amazing new feature","body":"Please pull these awesome changes in!","head":"integrate-github-api","base":"main"}"#;
+async fn call_gh_api(github_data: GitData, token: String, title: &String) -> reqwest::Result<String> {
+    let test_body = json!({
+        "title": title,
+        "body": "New pull request",
+        "head": github_data.branch,
+        "base": github_data.main_branch
+    });
 
     let client = reqwest::Client::new();
-    let resp = client.post("https://api.github.com/repos/MitchWijt/prBuddy/pulls")
+    let url = format!("https://api.github.com/repos/{}/{}/pulls", github_data.owner, github_data.repo_name);
+    // let body = r#"{"title":"Amazing new feature","body":"Please pull these awesome changes in!","head":"integrate-github-api","base":"main"}"#;
+
+    let resp = client.post(url)
         .header("Accept", "application/vnd.github+json")
         .header("Authorization", format!("Bearer {}", token))
         .header("User-Agent", format!("{}", github_data.repo_name))
         .header("X-GitHub-Api-Version", "2022-11-28")
-        .body(body)
+        .body(test_body)
         .send()
         .await?;
 
@@ -57,13 +66,14 @@ async fn call_gh_api(github_data: GitData, token: String) -> reqwest::Result<Str
     Ok(response)
 }
 
-fn get_url_from_response(response: String) -> serde_json::Result<()> {
+fn get_url_from_response(response: String) -> Result<String, &'static str> {
     let root: Value = serde_json::from_str(response.as_str())?;
 
     let url: Option<&str> = root.get("url")
         .and_then(|value| value.as_str());
 
-    println!("{:?}", url);
-
-    Ok(())
+    match url {
+        Some(value) => Ok(String::from(value)),
+        None => Err("Was not able to get URL from result")
+    }
 }
