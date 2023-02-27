d135a2f05d1ba008c85823b0047cd0be08d010c4
MitchWijt <mitchel@wijt.net>
2023-02-27 15:51:35 +0100

    feat: config data as env variables

--- src/config_data.rs
+++ src/config_data.rs
@@ -1,6 +1,7 @@
 use std::fs::{File, read_to_string};
 use std::{str};
 use std::io::{BufRead, BufReader};
+use std::env;
 
 pub struct Config {
     pub slack_webhook_url: String,
@@ -8,8 +9,8 @@ pub struct Config {
 }
 
 pub fn get_config_data() -> Result<Config, &'static str> {
-    let slack_webhook_url = get_value_from_config_file("SLACK_WEBHOOK_URL")?;
-    let github_api_key = get_value_from_config_file("GITHUB_API_KEY")?;
+    let slack_webhook_url = get_value_from_env("PR_BUDDY_SLACK_WEBHOOK_URL")?;
+    let github_api_key = get_value_from_env("PR_BUDDY_GH_API_KEY")?;
 
     return Ok(Config{
         slack_webhook_url,
@@ -17,23 +18,7 @@ pub fn get_config_data() -> Result<Config, &'static str> {
     })
 }
 
-fn get_value_from_config_file(env: &str) -> Result<String, &'static str> {
-    let file = File::open(".prBuddy").expect("Failed to open config file");
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        let result = line.expect("Failed to read line");
-
-        let split = result.split("=");
-        let vector: Vec<&str> = split.collect();
-
-        let config_variable = *vector.get(0).expect("Failed to grab config var");
-        let config_value = *vector.get(1).expect("Failed to grab config var value");
-
-        if config_variable == env {
-            return Ok(String::from(config_value))
-        }
-    }
-
-    Err("Failed to read config value")
+fn get_value_from_env(env_variable: &str) -> Result<String, &'static str> {
+    let value = env::var(env_variable).expect(&*format!("ENV variable missing: {}", env_variable));
+    return Ok(value)
 }
